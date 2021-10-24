4feaf9349c4e070896f78f77ac52356892902782
jmuminov <jmuminov92@gmail.com>
2021-10-24 14:57:24 -0400

    Cucumber Class 1 commit 3

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -12,7 +12,9 @@ public class LoginSteps extends CommonMethods {
     @When("user enters valid admin username and password")
     public void user_enters_valid_admin_username_and_password() {
         LoginPage loginPage = new LoginPage();
-        loginPage.login(ConfigReader.getPropertyValue("username"), ConfigReader.getPropertyValue("password"));
+        //loginPage.login(ConfigReader.getPropertyValue("username"), ConfigReader.getPropertyValue("password"));
+        sendText(loginPage.usernameBox, ConfigReader.getPropertyValue("username"));
+        sendText(loginPage.passwordBox, ConfigReader.getPropertyValue("password"));
     }
 
     @When("user clicks on login button")
@@ -24,25 +26,29 @@ public class LoginSteps extends CommonMethods {
     @Then("admin user is successfully logged in")
     public void admin_user_is_successfully_logged_in() {
         DashBoardPage dash = new DashBoardPage();
-        Assert.assertTrue((dash.welcomeMessage.isDisplayed()));
+        Assert.assertTrue(dash.welcomeMessage.isDisplayed());
     }
 
     @When("user enters valid ess username and password")
     public void user_enters_valid_ess_username_and_password() {
         LoginPage loginPage = new LoginPage();
-        loginPage.login("johnny1234560000", "Syntax1253!!!!");
+        //loginPage.login("johnny1234560000", "Syntax1253!!!!");
+        sendText(loginPage.usernameBox, "johnny1234560000");
+        sendText(loginPage.passwordBox, "Syntax1253!!!!");
     }
 
     @Then("ess user is successfully logged in")
     public void ess_user_is_successfully_logged_in() {
         DashBoardPage dash = new DashBoardPage();
-        Assert.assertTrue((dash.welcomeMessage.isDisplayed()));
+        Assert.assertTrue(dash.welcomeMessage.isDisplayed());
     }
 
     @When("user enters valid username and invalid password")
     public void user_enters_valid_username_and_invalid_password() {
         LoginPage loginPage = new LoginPage();
-        loginPage.login(ConfigReader.getPropertyValue("Admin"), "Syntax1253!!!!");
+        //loginPage.login(ConfigReader.getPropertyValue("Admin"), "Syntax1253!!!!");
+        sendText(loginPage.usernameBox, "Admin");
+        sendText(loginPage.passwordBox, "Syntax1253!!!!");
     }
 
     @Then("user see invalid credentials message on login page")
