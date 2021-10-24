1c89efd60d25c35ea4199a1b135a9e599d702761
jmuminov <jmuminov92@gmail.com>
2021-10-24 12:54:23 -0400

    Cucumber Class 1 commit

---
+++ src/test/java/steps/EmployeeSearchSteps.java
@@ -0,0 +1,50 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.DashBoardPage;
+import pages.EmployeeListPage;
+import pages.LoginPage;
+import utils.CommonMethods;
+import utils.ConfigReader;
+
+public class EmployeeSearchSteps extends CommonMethods {
+    @Given("user is navigated to HRMS")
+    public void user_is_navigated_to_hrms() {
+        openBrowser();
+    }
+
+    @Given("user logged in with valid admin credentials")
+    public void user_logged_in_with_valid_admin_credentials() {
+        LoginPage loginPage = new LoginPage();
+        /*sendText(loginPage.usernameBox, ConfigReader.getPropertyValue("username"));
+        sendText(loginPage.passwordBox, ConfigReader.getPropertyValue("password"));
+        click(loginPage.loginBtn);*/
+        loginPage.login(ConfigReader.getPropertyValue("username"), ConfigReader.getPropertyValue("password"));
+    }
+
+    @Given("user navigates to employee list page")
+    public void user_navigates_to_employee_list_page() {
+        DashBoardPage dash = new DashBoardPage();
+        click(dash.pimOption);
+        click(dash.employeeListOption);
+    }
+
+    @When("user enters valid employee id")
+    public void user_enters_valid_employee_id() {
+        EmployeeListPage emp = new EmployeeListPage();
+        sendText(emp.idEmployee, "20119000");
+    }
+
+    @When("click on search button")
+    public void click_on_search_button() {
+        EmployeeListPage emp = new EmployeeListPage();
+        click(emp.searchButton);
+    }
+
+    @Then("user see employee information is displayed")
+    public void user_see_employee_information_is_displayed() {
+        System.out.println("Info is displayed");
+    }
+}
---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,154 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class CommonMethods {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    public void openBrowser() {
+        ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        switch (ConfigReader.getPropertyValue("browser")) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("Invalid browser name");
+        }
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
+
+    public static void sendText(WebElement element, String textToSend) {
+        element.clear();
+        element.sendKeys(textToSend);
+    }
+
+    public static WebDriverWait getWait() {
+        WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICIT_WAIT);
+        return wait;
+    }
+
+    public static void waitForClickability(WebElement element) {
+        getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void click(WebElement element) {
+        waitForClickability(element);
+        element.click();
+    }
+
+    public static JavascriptExecutor getJSExecutor() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return js;
+    }
+
+    public static void jsClick(WebElement element) {
+        getJSExecutor().executeScript("arguments[0].click;", element);
+    }
+
+    public static void takeScreenshot(String fileName) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File sourceFile = ts.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(sourceFile, new File(Constants.SCREENSHOT_FILEPATH + fileName + " " + getTimeStamp("yyy-MM-dd-HH-mm-ss") + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getTimeStamp(String pattern) {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        return sdf.format(date);
+    }
+
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public static void setUpChrome(String url) {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
+
+    public static void setUpFirefox(String url) {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get(url);
+    }
+
+    public static void maximizeWindow() {
+        driver.manage().window().maximize();
+    }
+
+    public static void navigateToURL(String url) {
+        driver.navigate().to(url);
+    }
+
+    public static void navigateBack() {
+        driver.navigate().back();
+    }
+
+    public static void navigateForward() {
+        driver.navigate().forward();
+    }
+
+    public static void navigateRefresh() {
+        driver.navigate().refresh();
+    }
+
+    public static void getUrl(String url) {
+        driver.get(url);
+    }
+
+    public static String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public static String getTitle() {
+        return driver.getTitle();
+    }
+
+    public static void switchToWindow(String handle) {
+        driver.switchTo().window(handle);
+    }
+
+    public static void implicitlyWait10sec() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public static void explicitWait10secXpath(String xpath) {
+        wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
+
+    public static void closeTab() {
+        driver.close();
+    }
+
+    public static void quitBrowser() {
+        driver.quit();
+    }
+}
