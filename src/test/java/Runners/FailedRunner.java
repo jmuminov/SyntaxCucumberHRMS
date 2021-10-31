106c1a729a31297b030b6f91592b39705553c569
jmuminov <jmuminov92@gmail.com>
2021-10-31 11:36:23 -0400

    Cucumber Class 5 commit 1

--- src/test/java/Runners/FailedRunner.java
+++ src/test/java/Runners/FailedRunner.java
@@ -6,15 +6,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/",
+        features = "@target/failed.txt",
         glue = "steps",
-        dryRun = false,
+        //dryRun = false,
         monochrome = true,
-        tags = "@error",
-        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "rerun:target/failed.txt"}
+        plugin = {"pretty"}
         //pretty - prints all the steps in console
 )
 
-public class Smoke {
-
+public class FailedRunner {
 }
