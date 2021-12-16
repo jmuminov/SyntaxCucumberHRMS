080224dc6beb4b29d8ac43e129c03ec94d33c141
jmuminov <jmuminov92@gmail.com>
2021-12-15 19:33:12 -0500

    Jenkins Class Commit 3 common method updatee

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -24,10 +24,16 @@ public class CommonMethods {
         ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
         switch (ConfigReader.getPropertyValue("browser")) {
             case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.setHeadless(true);
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                if (ConfigReader.getPropertyValue("headless").equals("true")) {
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.setHeadless(true);
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(chromeOptions);
+                } else {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+
                 break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
